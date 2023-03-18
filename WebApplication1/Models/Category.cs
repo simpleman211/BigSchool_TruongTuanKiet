2618c3b46a9a04d616c8f6977f335d9f76504f26
User <User@LAPTOP-306CTLDL>
2023-03-18 12:37:41 +0700

    Add project files.

---
+++ WebApplication1/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Build.Framework;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+    
+namespace WebApplication1.Models
+{
+    public class Category
+    {
+        public byte Id { get; set; }
+        [Microsoft.Build.Framework.Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+    }
+}
---
+++ WebApplication1/ViewModels/CourseViewModel.cs
@@ -0,0 +1,29 @@
+﻿
+using Microsoft.Build.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication1.Models;
+
+namespace WebApplication1.ViewModels
+{
+    public class CourseViewModel
+    {
+        [Required]
+        public string Place { get; set; }
+        [Required]
+        [FutureDate]
+        public string Date { get; set; }
+        [Required]
+        [ValidTime]
+        public string Time { get; set; }
+        [Required]
+        public byte Category { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+        public DateTime GetDateTime() 
+        {
+            return DateTime.Parse(string.Format("{0} {1}", Date, Time)); 
+        }
+    }
+}
